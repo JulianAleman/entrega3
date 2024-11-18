e9f0553e9f6065aba2a2b09912b23748faa503fa
JulianAleman <jr.ramireza1@uniandes.edu.co>
2024-11-17 20:28:27 -0500

    RF1

--- demo/src/main/java/uniandes/edu/co/demo/controller/SucursalController.java
+++ demo/src/main/java/uniandes/edu/co/demo/controller/SucursalController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +30,15 @@ public class SucursalController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+    @PostMapping("/new/save")
+     public ResponseEntity<String> crearBar(@RequestBody Sucursal Sucursal) {
+        try {
+
+            sucursalRepository.insertarSucursal(Sucursal.getId(), Sucursal.getNombre(),Sucursal.getTamano(),Sucursal.getDireccion() , Sucursal.getTelefono(), Sucursal.getCiudad(), Sucursal.getOrden());
+            return new ResponseEntity<>("Bar creado exitosamente", HttpStatus.CREATED);
+        } catch (Exception e) {;
+            return new ResponseEntity<>("Error al crear el bar: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- demo/src/main/java/uniandes/edu/co/demo/repository/SucursalRepository.java
+++ demo/src/main/java/uniandes/edu/co/demo/repository/SucursalRepository.java
@@ -4,9 +4,14 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import uniandes.edu.co.demo.modelo.Ciudad;
+import uniandes.edu.co.demo.modelo.OrdenCompra;
 import uniandes.edu.co.demo.modelo.Sucursal;
 
 public interface SucursalRepository extends MongoRepository<Sucursal, Integer>{
     @Query(value="{}")
     List<Sucursal> buscarTodasLasSucursales();
+
+    @Query("{$insert:{_id:?0, Nombre:?1, Tamamo:?2, Direccion:?3, Telefono:?4, Ciudad: ?5, OrdenCompra:?6}}")
+    void insertarSucursal(int id, String Nombre, Double Tamano, String Direccion, String Telefono, Ciudad Ciudad, OrdenCompra OrdenCompra);
 }

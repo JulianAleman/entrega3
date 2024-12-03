daebe5213ce87d2ed397fb2211349632d83b43ea
Paula <p.morac@uniandes.edu.co>
2024-12-03 13:01:12 -0500

    mmm

--- demo/src/main/java/uniandes/edu/co/demo/repository/ProductoRepository.java
+++ demo/src/main/java/uniandes/edu/co/demo/repository/ProductoRepository.java
@@ -18,7 +18,7 @@ public interface ProductoRepository extends MongoRepository<Producto, Integer>{
     @Query(value="{}")
     List<Producto> buscarTodosLosProductos();
 
-    @Query("{ $set: { _id: ?0, Nombre: ?1, PrecioUnitarioVenta: ?2, Presentacion: ?3, CantidadPresentacion: ?4, UnidadMedida: ?5, FechaExpiracion: ?6, tipo: ?7, Categoria: ?8, EspecificacionEmpacado: ?9 }}")
+    @Query("{ $insertOne: { _id: ?0, Nombre: ?1, PrecioUnitarioVenta: ?2, Presentacion: ?3, CantidadPresentacion: ?4, UnidadMedida: ?5, FechaExpiracion: ?6, tipo: ?7, Categoria: ?8, EspecificacionEmpacado: ?9 }}")
     void insertarProducto(int _id, String Nombre, Double PrecioUnitarioVenta, String Presentacion, Integer CantidadPresentacion, String UnidadMedida, Date FechaExpiracion, String tipo, Categoria Categoria, EspecificacionEmpacado EspecificacionEmpacado);
 
     @Query("{ $or: [ { 'CodigoBarras': ?0 }, { 'Nombre': ?1 } ] }")
